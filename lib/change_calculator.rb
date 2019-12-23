f15fafb65144cd76ce5ea73a2a27aeb38cd5b67b
Sergio Gernyak <sergeg1990@gmail.com>
2019-12-23 23:00:10 +0200

    Implement the change calculator

--- lib/change_calculator.rb
+++ lib/change_calculator.rb
@@ -1,14 +1,25 @@
 class ChangeCalculator
+  Result = Struct.new(:full_change, :coins, :change_amount)
+
   def initialize(change_amount, coins)
     @change_amount = change_amount
     @coins = coins
   end
 
   def calculate
-    Array.new(6, nil)
+    change_coins = []
+    coin_values = @coins.map(&:value).uniq.sort { |a, b| b <=> a }
+    while coin_values.count.positive? && @change_amount.positive?
+      coin_value = coin_values.first
+      coin = @coins.find { |c| c.value == coin_value }
+      if coin && (coin_value <= @change_amount)
+        @change_amount -= coin_value
+        change_coins << coin.dup
+        @coins.delete_at(@coins.index(coin))
+      else
+        coin_values = coin_values[1..-1]
+      end
+    end
+    Result.new(@change_amount.zero?, change_coins, @change_amount)
   end
-
-  private
-
-  attr_reader :change_amount, :coins
 end
--- test/change_calculator_test.rb
+++ test/change_calculator_test.rb
@@ -18,15 +18,47 @@ class ChangeCalculatorTest < Minitest::Test
       '50c' => 4,
       '2' => 1
     }
-    coins = coins_in_bank.inject([]) do |coins, (coin_code, amount)|
-      coins.concat(Array.new(amount, coin_factory.build_by_code(coin_code)))
+    coins = coins_in_bank.inject([]) do |accumulator, (coin_code, amount)|
+      accumulator.concat(Array.new(amount, coin_factory.build_by_code(coin_code)))
     end
     change_calculator = ChangeCalculator.new(
       425,
       coins
     )
-    change_coins = change_calculator.calculate
-    assert_equal 6, change_coins.count
+
+    change_result = change_calculator.calculate
+
+    assert_equal true, change_result.full_change
+    assert_equal 6, change_result.coins.count
+
+    grouped_change_coins = change_result.coins.group_by { |c| c.code }
+    assert_equal 1, grouped_change_coins['2'].count
+    assert_equal 4, grouped_change_coins['50c'].count
+    assert_equal 1, grouped_change_coins['25c'].count
+  end
+
+  def test_not_enough_coins_to_cover_the_change
+    coins_in_bank = {
+      '50c' => 4,
+      '2' => 1
+    }
+    coins = coins_in_bank.inject([]) do |accumulator, (coin_code, amount)|
+      accumulator.concat(Array.new(amount, coin_factory.build_by_code(coin_code)))
+    end
+    change_calculator = ChangeCalculator.new(
+      425,
+      coins
+    )
+
+    change_result = change_calculator.calculate
+
+    assert_equal false, change_result.full_change
+    assert_equal 5, change_result.coins.count
+    assert_equal 25, change_result.change_amount
+
+    grouped_change_coins = change_result.coins.group_by { |c| c.code }
+    assert_equal 1, grouped_change_coins['2'].count
+    assert_equal 4, grouped_change_coins['50c'].count
   end
 
   private
